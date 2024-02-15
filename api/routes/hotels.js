261dc7df3025c5217d0f6b8b6f59414d415dfd5c
SandhyaR1007 <grajwanshi1008@gmail.com>
2024-02-15 22:16:59 +0530

    update: added error handler middleware

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -3,16 +3,16 @@ import Hotels from "../models/Hotel.js";
 
 const hotelsRouter = express.Router();
 
-hotelsRouter.post("/addHotel", async (req, res) => {
+hotelsRouter.post("/addHotel", async (req, res, next) => {
   const newHotel = new Hotels(req.body);
   try {
     const savedHotel = await newHotel.save();
     res.status(201).json({ message: "Hotel Saved Successfully", savedHotel });
   } catch (error) {
-    res.status(500).json(error);
+    next(error);
   }
 });
-hotelsRouter.put("/updateHotel/:id", async (req, res) => {
+hotelsRouter.put("/updateHotel/:id", async (req, res, next) => {
   try {
     const updatedHotel = await Hotels.findByIdAndUpdate(
       req.params.id,
@@ -21,25 +21,25 @@ hotelsRouter.put("/updateHotel/:id", async (req, res) => {
     );
     res.status(200).json({ message: "Hotel Updated", updatedHotel });
   } catch (error) {
-    res.status(500).json(error);
+    next(error);
   }
 });
 
-hotelsRouter.get("/", async (req, res) => {
+hotelsRouter.get("/", async (req, res, next) => {
   try {
     const hotels = await Hotels.find();
     res.status(200).json({ message: "Hotels found", hotels });
   } catch (error) {
-    res.status(500).json({ error });
+    next(error);
   }
 });
 
-hotelsRouter.delete("/deleteHotel/:id", async (req, res) => {
+hotelsRouter.delete("/deleteHotel/:id", async (req, res, next) => {
   try {
     const hotel = await Hotels.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "Hotel deleted", hotel });
   } catch (error) {
-    res.status(500).json({ error });
+    next(error);
   }
 });
 
