0a5b070c56daa8db7e52bc084a7d91332989bb58
SandhyaR1007 <grajwanshi1008@gmail.com>
2024-02-18 14:25:18 +0530

    update: user register and login, encryption of password

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,3 +1,38 @@
-const register = async (req, res, next) => {};
+import bcrypt from "bcryptjs";
 
-export { register };
+import Users from "../models/User.js";
+import { createError } from "../utils/error.js";
+
+const register = async (req, res, next) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  try {
+    const newUser = new Users({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User Created Successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const user = await Users.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User Not Found"));
+    bcrypt.compare(req.body.password, user.password, function (err, match) {
+      if (match) {
+        res.status(200).json({ message: "Login Successful" });
+      } else {
+        next(createError(401, "Unauthorized"));
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { register, login };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js";
 
 const authRouter = express.Router();
 
-authRouter.get("/", register);
+authRouter.post("/register", register);
+authRouter.post("/login", login);
 
 export default authRouter;
