e7870b9cde9203d2763d37ee29b1acf189dc9a1b
SandhyaR1007 <grajwanshi1008@gmail.com>
2024-02-19 21:56:46 +0530

    update: verification for user operations

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,23 @@
 import express from "express";
+import {
+  deleteUserById,
+  getAllUsers,
+  getUserById,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const usersRouter = express.Router();
 
-usersRouter.get("/", (req, res) => {
-  res.send("hello from users");
-});
+//GET ALL
+usersRouter.get("/", verifyAdmin, getAllUsers);
+
+//GET
+usersRouter.get("/:id", verifyUser, getUserById);
+
+//UPDATE
+usersRouter.put("/:id", verifyUser, updateUser);
+//DELETE
+usersRouter.delete("/:id", verifyUser, deleteUserById);
 
 export default usersRouter;
