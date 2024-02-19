4d4ae4c1530299d6ed385b2d9410b11985ef373b
SandhyaR1007 <grajwanshi1008@gmail.com>
2024-02-19 21:56:19 +0530

    add: verification for user and handle authorisation

---
+++ api/utils/verifyToken.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if (!token) {
+    return next(createError(404, "Token not found"));
+  }
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) {
+      return next(createError(403, "Invalid Token"));
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
+
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user._id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      next(createError(403, "Unauthorized"));
+    }
+  });
+};
+
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
